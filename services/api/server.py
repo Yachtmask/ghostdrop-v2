090537cfa9d028672a58662310ea79dc029e4b56
sujalle <111200080+Yachtmask@users.noreply.github.com>
2026-03-15 19:36:29 +0100

    Update server.py

--- services/api/server.py
+++ services/api/server.py
@@ -9,9 +9,23 @@ def wallet():
     wallet_address = data.get("address")
 
     return jsonify({
-        "status": "received",
+        "status": "wallet received",
         "wallet": wallet_address
     })
+
+
+@app.route("/drop", methods=["POST"])
+def drop():
+
+    data = request.json
+    vault = data.get("vault")
+
+    print("Vault received")
+
+    return jsonify({
+        "status": "vault stored"
+    })
+
 
 if __name__ == "__main__":
     app.run(port=5000)
